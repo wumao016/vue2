a2b64864ba591fdc09a180c9a4bc0d244d22c3ed
wumao016 <182075532@qq.com>
2018-06-06 21:01:13 +0800

    在线翻译

---
+++ fanyi/src/components/TranslateForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="row" id="translateForm">
+    <div class="col-md-6 col-md-offset-3 well">
+      <form id="transForm" class="form-inline" v-on:submit="formSubmit">
+        <input class="form-control" type="text" v-model="textToTranslate" placeholder="输入需要翻译的内容">
+        <select class="form-control" v-model="language">
+          <option value="en">English</option>
+          <option value="ru">Russian</option>
+          <option value="ko">Korean</option>
+          <option value="ja">Janpenese</option>
+        </select>
+        <input class="btn btn-primary" type="submit" value="翻译">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'translateForm',
+  data:function(){
+    return {
+      textToTranslate:"",
+      language:""
+    }
+  },
+  methods: {
+    formSubmit:function(e){
+      // alert(this.textToTranslate);
+      this.$emit("formSubmit",this.textToTranslate,this.language);
+      e.preventDefault();
+    }
+  },
+  created:function(){
+    this.language = "en";
+  }
+}
+</script>
+
+<style>
+  .well{
+    margin: 0 25%;
+  }
+</style>
